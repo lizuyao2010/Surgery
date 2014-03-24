021c1a389e34eb28e4c66a92cbdb91ceb9be5ee9
lizuyao2010 <zuyaoli2010@gmail.com>
2014-03-24 14:19:44 +0100

    add test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import sparql
+endpoint='http://localhost:8080/openrdf-workbench/repositories/AAOT/query'
+statement=("""PREFIX aaot:<http://cs.lth.se/ontologies/aaot.owl#>
+	select * WHERE {
+	 ?id aaot:age ?age . 
+	 ?id aaot:age_donor ?age_donor . 
+	 ?id aaot:gender ?gen . }"""
+	 )
+result = sparql.query(endpoint,statement)
+print result.variables
+for row in result.fetchall():
+   #print 'row:',row
+   values=sparql.unpack_row(row)
+   ID=values[0].split('#')[1]
+   print ID, 'age:',values[1], 'age_donor:',values[2], 'gen:',values[3]
