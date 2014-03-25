7e01e59dd473f4117a390c9d36922f8108a39e92
lizuyao2010 <zuyaoli2010@gmail.com>
2014-03-25 12:49:22 +0100

    add data

--- test.py
+++ test.py
@@ -1,21 +1,35 @@
 # -*- coding: utf-8 -*-
 import sparql
-endpoint='http://130.235.17.116:8000/openrdf-workbench/repositories/AAOT/query'
-statement=("""PREFIX aaot:<http://cs.lth.se/ontologies/aaot.owl#>
-	select * WHERE {
-         ?name aaot:transplant_id ?transplant_id .
-	 ?name aaot:age ?age . 
-	 ?name aaot:age_donor ?age_donor . 
-	 ?name aaot:gender ?gen . 
-         ?name aaot:survival_time ?survival_time .
-         }"""
-	 )
-result = sparql.query(endpoint,statement)
-variables = result.variables
-for row in result.fetchall():
-   values=sparql.unpack_row(row)
-   i=1
-   while i<len(values):
-      print variables[i],values[i],'\t',
-      i+=1
-   print 
+import os.path
+
+def print_result(offset,f):
+   endpoint='http://130.235.17.116:8000/openrdf-sesame/repositories/AAOT'
+   statement=("""PREFIX aaot:<http://cs.lth.se/ontologies/aaot.owl#>
+   	select * WHERE {
+            ?name aaot:transplant_id ?transplant_id .
+   	 ?name aaot:age ?age . 
+   	 ?name aaot:age_donor ?age_donor . 
+   	 ?name aaot:gender ?gen . 
+            ?name aaot:survival_time ?survival_time .
+            } limit 1000
+            offset """
+            +str(offset)
+   	 )
+   result = sparql.query(endpoint,statement)
+   variables = result.variables
+   for row in result.fetchall():
+      values=sparql.unpack_row(row)
+      i=1
+      while i<len(values):
+         print >> f,variables[i],values[i],'\t',
+         i+=1
+      print >> f
+if __name__=='__main__':
+   offset=0
+   if not (os.path.isfile('./data.txt')):
+      f=open('data.txt','w')
+      while offset<100000:
+         print_result(offset,f)
+         offset+=1000
+   else:
+      print "nothing"
